831e8bc95f7f314f5294a52522c98f6898dfbcf8
binhex <megalith01@gmail.com>
2014-07-17 12:47:04 +0100

    check env variable, abs path filebot, partial remove

--- get_iplayer-script.sh
+++ get_iplayer-script.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
-# Script to download bbc iplayer tv series
+# Script to download bbc iplayer tv series, also includes filebot renaming
 
 #set the input field separator
 IFS=','
 
-#create list of bbc iplayer shows to download
-show_list="Everything's Rosie,Waybuloo,Chuggington,Mike the Knight,The Adventures of Abney and Teal,The Adventures of Abney & Teal,Tree Fu Tom,Octonauts,Charlie and Lola,Little Robots,Q Pootle 5,Topsy and Tim"
+#check to make sure env set and is not empty - defined via run command
+if [ ${SHOWLIST:+x} ]
 
-#loop over list of shows
-for show_name in $show_list;
+	then echo "TV show list defined as ($SHOWLIST), looping over list..."
+	else echo "TV show list is not defined and/or is blank, please specify using the run command for the docker using the -e flag, see docker site for details"
+	
+fi
+
+#loop over list of shows - show list set via env variable in run command
+for show_name in $SHOWLIST;
 
 	do
-
+	
+	#delete any partial files
+	rm -rf "/media/$show_name/*partial*"
+	
 	#run get_iplayer for each show
 	/usr/bin/get_iplayer --profile-dir /config --get --modes=flashhd,flashvhigh,flashhigh,flashstd,flashnormal,flashlow --file-prefix="$show_name - <senum> - <episodeshort>" "$show_name" --output "/media/$show_name"
 
 	if [ -d "$show_name" ]; then
 
 		#run filebot to rename downloaded tv episode
-		filebot -rename "/media/$show_name" --action move --format "/media/{n}/Season {s.pad(2)}/{n} - {s.pad(2)}x{e.pad(2)} - {t}" --db thetvdb --conflict skip --log all
+		/usr/bin/filebot -rename "/media/$show_name" --action move --format "/media/{n}/Season {s.pad(2)}/{n} - {s.pad(2)}x{e.pad(2)} - {t}" --db thetvdb --conflict skip --log all
 	fi
 
 done
