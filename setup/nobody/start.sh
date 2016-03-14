d5d1886871ea89b54af2aaf6033b6ba88fbc0fab
binhex <megalith01@gmail.com>
2016-03-14 08:12:42 +0000

    additional logging, whitespace removal from shows

--- setup/nobody/start.sh
+++ setup/nobody/start.sh
@@ -4,11 +4,11 @@
 # if SHOWS env var not defined then exit
 if [[ -z "${SHOWS}" ]]; then
 
-	echo "TV show list is not defined and/or is blank, please specify shows to download using the environment variable SHOWS"
+	echo "[crit] TV show list is not defined and/or is blank, please specify shows to download using the environment variable SHOWS"
 
 else
 
-	echo "TV shows defined as ${SHOWS}"
+	echo "[info] TV shows defined as ${SHOWS}"
 
 fi
 
@@ -18,24 +18,28 @@ umask 000
 # split comma seperated string into list from SHOW env variable
 IFS=',' read -ra SHOWLIST <<< "${SHOWS}"
 
+# make folder for incomplete downloads
+mkdir -p "/data/get_iplayer/incomplete"
+
+# make folder for completed downloads
+mkdir -p "/data/completed"
+
 # loop over list of shows with scheduled sleep period
 while true
 do
 
-	# make folder for incomplete downloads
-	mkdir -p "/data/get_iplayer/incomplete"
-
-	# make folder for completed downloads
-	mkdir -p "/data/completed"
-
-	# loop over list of shows
+	# process each show in the list
 	for show_name in "${SHOWLIST[@]}"; do
 
-		echo "Processing show ${show_name}..."
+		# strip whitespace from start and end of show_name
+		show_name=$(echo "${show_name}" | sed -e 's/^[ \t]*//')
+		
+		echo "[info] Processing show ${show_name}..."
 
-		echo "Delete partial downloads from incomplete folder..."
+		echo "[info] Delete partial downloads from incomplete folder..."
 		find /data/get_iplayer/incomplete/ -type f -name "*partial*" -delete
 		
+		echo "[info] Running get_iplayer..."
 		# run get_iplayer for show, saving to incomplete folder
 		/usr/bin/get_iplayer --profile-dir /config --get --nopurge --modes=flashhd,flashvhigh,flashhigh,flashstd,flashnormal,flashlow --file-prefix="${show_name} - <senum> - <episodeshort>" "${show_name}" --output "/data/get_iplayer/incomplete/${show_name}"
 
@@ -44,27 +48,29 @@ do
 	# check incomplete folder DOES contain files with flv extension
 	if [[ -n $(find /data/get_iplayer/incomplete/ -name '*.flv') ]]; then
 
-		echo "Copying folders in incomplete folder to completed..."
+		echo "[info] Copying show folders in incomplete to completed..."
 		cp -rf "/data/get_iplayer/incomplete"/* "/data/completed/"
 
 		# if copy succesful then delete show folder in incomplete folder
 		if [[ $? -eq 0 ]]; then
 
-			echo "Delete folders in incomplete after succesful copy..."
+			echo "[info] Copy succesful, deleting incoomplete folders..."
 			rm -rf "/data/get_iplayer/incomplete"/*
-
+		else
+			echo "[error] Copy failed, skipping deletion of show folders in incoomplete folder..."
 		fi
 
 	fi
 
 	# if env variable SCHEDULE not defined then use default
 	if [[ -z "${SCHEDULE}" ]]; then
-
+	
+		echo "[info] Env var SCHEDULE not defined, sleeping for 12 hours..."
 		sleep 12h
 
 	else
-
-		sleep ${SCHEDULE}
+		echo "[info] Env var SCHEDULE defined, sleeping for ${SCHEDULE}..."
+		sleep "${SCHEDULE}"
 
 	fi
 
