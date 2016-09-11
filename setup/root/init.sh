4e7cc7a3e6109ea3468bf383ad55efc96e9da345
binhex <megalith01@gmail.com>
2016-09-11 22:21:27 +0100

    config supervisor for pid 1

--- setup/root/init.sh
+++ setup/root/init.sh
@@ -44,4 +44,4 @@ chmod -R 775 /usr/share/get_iplayer/plugins /usr/bin/get_iplayer /home/nobody
 echo "[info] Starting Supervisor..."
 
 # run supervisor
-"/usr/bin/supervisord" -c "/etc/supervisor.conf" -n
+exec /usr/bin/supervisord -c /etc/supervisor.conf -n
