4d89c49cb8d6f2047768198c1406184a3e598c8f
binhex <megalith01@gmail.com>
2025-10-07 10:04:11 +0100

    switch to new aur.sh

--- build/root/install.sh
+++ build/root/install.sh
@@ -55,10 +55,10 @@ fi
 ####
 
 # define aur packages
-aur_packages="flvstreamer get_iplayer"
+aur_packages="flvstreamer,get_iplayer"
 
 # call aur install script (arch user repo)
-source aur.sh
+aur.sh --aur-package "${aur_packages}"
 
 # container perms
 ####
